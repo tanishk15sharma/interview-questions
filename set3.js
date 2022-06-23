41c660ab635fa54cf7a7c040fb986ecc67831111
tanishk15sharma <tanishk1582002@gmail.com>
2022-06-24 01:27:11 +0530

    feat:day12

--- set3.js
+++ set3.js
@@ -124,8 +124,65 @@ const findFrequentNum = (arr) => {
   return mostFrequentNum;
 };
 
-console.log(
-  findFrequentNum([2, 4, 6, 9, 8, 5, 3, 2, 6, 7, 8, 3, 2, 6, 7, 7, 6, 2, 7])
-);
+// console.log(findFrequentNum([2, 4, 6, 9, 3, 2, 6, 7, 8, 2, 6, 7, 7, 6, 2, 7]));
 
 // -------------------------------------------------------------------------------------------
+
+//Given an array of integer nums and an integer target, check if there are two numbers in the array such that they add up to target. Return any one pair that add up to the target
+
+const checkTargetPair = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        // result.push(arr[i], arr[j]); this is also right
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+  return -1;
+};
+
+// console.log(checkTargetPair([3, 7, 11, 15], 18));
+
+//Given an array and size k, break the array into chunks of k-length and return them as an array.
+
+const chunksOfArr = (arr, chunkLength) => {
+  let result = [];
+  let tempArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (tempArr.length === chunkLength) {
+      result.push(tempArr);
+      tempArr = [];
+    }
+    tempArr.push(arr[i]);
+  }
+  return result;
+};
+
+// console.log(chunksOfArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3));
+
+const findLeastFrequentNum = (arr) => {
+  const numFrequencyStorage = {};
+
+  for (let num of arr) {
+    if (numFrequencyStorage[num]) {
+      numFrequencyStorage[num]++;
+    } else {
+      numFrequencyStorage[num] = 1;
+    }
+  }
+
+  const output = { num: 0, count: arr.length };
+
+  for (let key in numFrequencyStorage) {
+    if (numFrequencyStorage[key] < output.count) {
+      output.count = numFrequencyStorage[key];
+      output.num = key;
+    }
+  }
+  return output;
+};
+
+// console.log(findLeastFrequentNum([2, 2, 3, 4, 4, 4, 2, 5, 5, 5, 6, 6, 8, 8]));
+
+// --------------------------------------------------------------------------------
